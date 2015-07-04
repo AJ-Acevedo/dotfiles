ad14cf2efea92786ea8cf520f73efd1b9b0b4286
AJ Acevedo <aj@ajalabs.com>
2015-07-04 19:39:24 -0400

    Finished the Homebrew installer and updater script

--- init/brew.sh
+++ init/brew.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 #
+# AJAlabs.com
+# brew install script
+# Homebrew installer and updater
+# v0.2
+#
 # Homebrew
 # http://brew.sh/
 #
-# This installs Homebrew and some common dependencies using.
 
-# Check if Homebrew is installed
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Set counter to Zero
+count=0
+
+# Exit unless host OS is Mac OS X
+if [ `uname -s` != "Darwin" ]; then
+  echo -e "\nThis script will only run on Mac OS X"
+  echo "exiting"
+  exit 1
 fi
 
-# Install homebrew packages
-brew install node sqlite tree wget 
+# Install Homebrew unless it already exists
+if ! brew -v > /dev/null 2>&1; then
+  echo -e "\n  Installing Homebrew...\n"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  count=1
+fi
+
+# Install packages if Homewbrew was just installed
+if [ $count -eq 1 ]; then
+  brew install git-lfs node python readline sqlite tree wget
+fi
+
+# If Homebrew already exists, update brew and all packages
+if [ $count -eq 0 ]; then
+  echo -e "\n Checking for Homebrew updates"
+  echo -e " Please wait while things are brewing..."
+  brew update && brew upgrade
+fi
 
 exit 0
--- init/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [ `uname -s` == "Darwin" ]; then
-  echo "Yup Darwin"
-
-fi
